c3b878c64c5e9f6a864e73b0efb7d525d54033b2
The Epic Hero <63442799+TheIndianHacker-crypto@users.noreply.github.com>
2020-04-15 00:30:30 +0530

    Add project files.

---
+++ PartialViewInAspMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PartialViewInAspMVC.Startup))]
+namespace PartialViewInAspMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
